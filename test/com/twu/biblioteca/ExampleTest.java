14aad33a66c6093c7c10190f9f18906881fe8b7e
Katie Larson <katielarson@nalsanchez.corporate.thoughtworks.com>
2019-04-12 12:10:56 -0500

    Add hamcrest dependency

--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,8 +1,9 @@
 package com.twu.biblioteca;
 
-
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
 
 public class ExampleTest {
 
