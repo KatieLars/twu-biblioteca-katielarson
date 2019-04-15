c151183802763e6725444b2851598c6b9d018354
Katie Larson <katielarson@nalsanchez.corporate.thoughtworks.com>
2019-04-15 11:30:25 -0500

    add Menu and MenuTest class

---
+++ test/com/twu/biblioteca/MenuTest.java
@@ -0,0 +1,13 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
+public class MenuTest {
+    @Test
+    public void shouldShowOptions() {
+        
+    }
+}
+
